70bef18b96f358e46911049fcf09d7c17479f832
Claudinei Machado <cjchamado@gmail.com>
2026-08-25 10:19:06 -0300

    add products

---
+++ apps/api/database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'category_id' => Category::factory(),
+            'name' => fake()->sentence(),
+            'description' => fake()->text(),
+            'price' => fake()->randomFloat(2, 100, 2000),
+            'enabled' => fake()->randomElement([true, false]),
+            'stock' => fake()->randomDigitNotNull(),
+            'position' => fake()->numberBetween(1, 100),
+        ];
+    }
+}
